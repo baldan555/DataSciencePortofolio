58b3618c94c4b2e40ae62734e9f6dd76c67986ee
baldan555 <64614565+baldan555@users.noreply.github.com>
2024-09-03 12:59:30 +0700

    Update AllInOne.py

--- AllInOne.py
+++ AllInOne.py
@@ -41,8 +41,93 @@ if selected_nav == "About Me":
     st.write("Deskripsi tentang Anda di sini.")
     
 elif selected_nav == "CV":
-    st.title("CV")
-    # Your CV content here...
+     # Profile Section with tighter spacing
+    st.markdown("""
+    # <div style='text-align: center; margin-bottom: -15px;'>N.I.S. BALDANULLAH</div>
+    ### <div style='text-align: center; margin-bottom: -10px;'>Data Enthusiast</div> 
+    <div style='text-align: center; margin-bottom: -10px;'>Jakarta, Indonesia</div>  
+    <div style='text-align: center;'>+6281245941155 | baldanullah55@gmail.com</div>
+    """, unsafe_allow_html=True)
+
+    # Add some spacing before the next section
+    st.write(" ")
+
+    # Profile Text
+    st.markdown("""
+    ## <div style='text-align: center; background-color: #f0f0f0; padding: 5px;'>PROFILE</div>
+    <hr style="border:2px solid black; margin-top: -40px;">
+    <p style='text-align: justify;'>
+    I am an enthusiastic fan of technology in the field of data science, studying informatics engineering at Pancasila University. 
+    The vast potential in data science has attracted my interest, especially its accurate ability to solve various problems in our society. 
+    So far, by utilizing the knowledge from the campus and by self-learning using various free resources and content I have been able to gain practical experience and apply the knowledge I have, with my abilities, talents, and enthusiasm. 
+    I believe that I am suitable to participate together we can drive meaningful progress both within the organization and in society at large.
+    </p>
+    """, unsafe_allow_html=True)
+
+    # Add some spacing
+    st.write(" ")
+
+    # Experience Section
+    st.markdown("""
+    ## <div style='text-align: center; background-color: #f0f0f0; padding: 5px;'>EXPERIENCE</div>
+    <hr style="border:2px solid black; margin-top: -40px;;">
+
+    #### **Big Data Analyst**  
+    _Aug 2023 — Dec 2023 | Makassar_  
+    - Managing large scale data such as cleansing, storing, and pre-processing data  
+    - Building Machine Learning models such as LSTM (Long Short Term Memory), GRU, Convolutional Neural Network for Predicting Asia Bitumen Price  
+    - Build and deploy machine learning model to predict and visualize an Asia Bitumen price into an interactive web app dashboard
+
+    <br>
+
+    #### **Website Administrator, Editor and Publisher at PT. INIPASTI Communika**  
+    _Jan 2020 — Dec 2022 | Makassar_  
+    - Editing journalists' news narratives  
+    - Rewriting news articles from journalists for publication on the company’s online media platform and overseeing the company’s media website  
+    - Monitoring website traffic  
+    - Keeping track of and managing the number of visitors to the website  
+    - Analyzing data insights  
+    - Examining and interpreting data to gain valuable insights
+
+    <br>
+
+    #### **Data Entry Assistant at Institute Social and Political Economic Issue (ISPEI)**  
+    _Jan 2014 — Dec 2016 | Makassar_  
+    - Developed and maintained data entry guidelines and processes to ensure accuracy and consistency  
+    - Input data from paper sheets into excel and cleaning and managing data
+    """, unsafe_allow_html=True)
+
+    # Add some spacing
+    st.write(" ")
+
+    # Education Section
+    st.markdown("""
+    ## <div style='text-align: center; background-color: #f0f0f0; padding: 5px;'>EDUCATION</div>
+    <hr style="border:2px solid black; margin-top: -40px;">
+
+    #### **Pancasila University**  
+    _Bachelor_  
+    _Mar 2020 — Aug 2024 | South Jakarta_
+    """, unsafe_allow_html=True)
+
+    # Add some spacing
+    st.write(" ")
+
+    # Project Section
+    st.markdown("""
+    ## <div style='text-align: center; background-color: #f0f0f0; padding: 5px;'>PROJECTS</div>
+    <hr style="border:2px solid black; margin-top: -40px;">
+
+    #### **Capstone Project**  
+    _Jun 2023 — Jun 2023 | Bangkit Academy_  
+    Building Machine Learning models to detect vehicle tire quality in vehicle service online booking applications with image processing using CNN (Convolutional Neural Network)
+
+    <br>
+
+    #### **Interactive Predictive FOB Bitumen Price Dashboard**  
+    _Sep 2024 — Nov 2023 | Kalla Group_  
+    Build and deploy machine learning model to predict and visualize an Asia bitumen price into an interactive web app dashboard
+    """, unsafe_allow_html=True)
 
 elif selected_nav == "Portofolio":
     with st.expander("Choose Category", expanded=True):
